dfdcca3574ba2132694e701c3bbca4f891760f38
Vanshika <er.vanshika18@gmail.com>
2025-05-17 00:12:34 +0530

    Updated IR structure and generator logic

--- IR/ir_generator.hpp
+++ IR/ir_generator.hpp
@@ -1,17 +1,44 @@
 #pragma once
-#include "Parser/ast.hpp"
-#include "ir.hpp"
 #include <string>
-#include <vector>
 #include <unordered_map>
+#include "ir.hpp"
+
+// Forward declarations of AST nodes
+class ASTNode;
+class ReturnNode;
+class VariableNode;
+class DeclarationNode;
+class VarDeclareNode;
+class NumberLiteralNode;
+class StringLiteralNode;
+class IfStatementNode;
+class ComparisonNode;
+class LogicalExprNode;
+class BinaryExprNode;
+class UnaryExprNode;
+class AssignmentNode;
+class PrintNode;
+class FunctionNode;
+class CallExprNode;
+class WhileNode;
+class BlockNode;
 
 class IRGenerator {
-public:
-    IR generate(const ASTNode* root);
-
-private:
-    IR instructions;
+    IR ir;
+    int labelCounter = 0;
     int tempVarCounter = 0;
-    std::string generateTemp();
-    std::string generateFromNode(const ASTNode* node);
+
+    std::unordered_map<std::string, std::string> symbolTable;
+
+    std::string newLabel();
+    std::string newTemp();
+
+    std::string generateExpression(ASTNode* node);
+
+public:
+    IRGenerator() = default;
+
+    void generate(ASTNode* node);
+    const IR& getIR() const;
 };
+
